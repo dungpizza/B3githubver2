5f78d9faadaa22c67a78b528422a25a3fc2bbd05
dungpizza <dung96hb@gmail.com>
2018-04-22 11:00:06 +0700

    commit changed

---
+++ app/src/main/java/com/example/dungcon/b3githubver2/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.dungcon.b3githubver2;
+
+import android.content.pm.ApplicationInfo;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dungcon on 4/22/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/dungcon/b3githubver2/Student.java
@@ -0,0 +1,8 @@
+package com.example.dungcon.b3githubver2;
+
+/**
+ * Created by dungcon on 4/22/2018.
+ */
+
+public class Student {
+}
